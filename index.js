d58406c0bad09a90428c47a45b03f4ff2331f4ad
Quinton Fults <quinton.fults.work@gmail.com>
2021-03-04 11:02:13 -0800

    code

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const dog = {
-  name: 'Beef',
-  age: 2,
-  breed: 'Chihuahua',
-  child: {
-    name: 'Chicken',
-    age: 1,
-    breed: 'Chihuahua'
-  },
-  bark () {
-    console.log(`Woof! My name is ${this.name}`)
-  }
-}
+// const dog = {
+//   name: 'Beef',
+//   age: 2,
+//   breed: 'Chihuahua',
+//   child: {
+//     name: 'Chicken',
+//     age: 1,
+//     breed: 'Chihuahua'
+//   },
+//   bark () {
+//     console.log(`Woof! My name is ${this.name}`)
+//   }
+// }
 
 // let { bark } = dog
 
@@ -27,3 +27,13 @@ const dog = {
 //  } = dog
 
 // console.log(dogAge)
+
+// let movies = ['Goodfellas', 'The Room', 'movie3', 'movie4', 'movie5']
+
+// let [, hotdog, , movie4 ] = movies
+
+// console.log(hotdog, movie4)
+
+// let [movie1, , , , movie5] = movies
+
+// console.log(movie1, movie5)
