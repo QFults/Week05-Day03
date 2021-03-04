a98970dac9b08bc1a797b7f9b8dab59b0d07b62a
Quinton Fults <quinton.fults.work@gmail.com>
2021-03-04 11:45:59 -0800

    code

--- index.js
+++ index.js
@@ -37,3 +37,29 @@
 // let [movie1, , , , movie5] = movies
 
 // console.log(movie1, movie5)
+
+let user = {
+  name: 'John Doe',
+  email: 'johndoe@gmail.com',
+  username: 'johndoe',
+  password: 'password1234',
+  address: '123 Main St',
+  phone: '(123) 456-7890'
+}
+
+function User(name, email, username, password, address, phone) {
+  this.name = name
+  this.email = email
+  this.username = username
+  this.password = password
+  this.address = address
+  this.phoneNumber = phone
+  this.ssn = 123456789
+}
+
+let newUser = new User('John Doe', 'johndoe@gmail.com', 'johndoe', 'password1234', '123 Main St', '(123) 456-7890')
+
+let newUser1 = new User()
+
+console.log(newUser.phoneNumber)
+console.log(newUser1.ssn)
